125c78195673cfc5a6f7a4fd20eb4191ec866e6f
notsheepdog <frauenfelder.j@northeastern.edu>
2023-02-05 11:48:18 -0500

    Adding initial unity project

---
+++ Friendly Fest/Assets/SampleScene/CameraControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float speed;
+    Vector3 pivot = Vector3.zero;
+
+    void Update()
+    {
+        transform.LookAt(pivot);
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+}
---
+++ Friendly Fest/Assets/SampleScene/PillDropper.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillDropper : MonoBehaviour
+{
+    public GameObject pill;
+    public float interval;
+
+    private bool dropping = false;
+
+    void Update()
+    {
+        if (!dropping)
+        {
+            dropping = true;
+            Invoke("resetDrop", interval);
+            GameObject p = Instantiate(pill, transform);
+            p.transform.eulerAngles = new Vector3(-5, Random.Range(0, 360), 0);
+
+            float lowC = 0.6f;
+            float hiC = 1f;
+            int cB = Random.Range(1, 7);
+
+            p.transform.GetChild(0).GetComponent<Renderer>().material.color = 
+                new Color(
+                    (cB == 1 || cB == 4 || cB == 5 ? lowC : hiC), 
+                    (cB == 2 || cB == 4 || cB == 6 ? lowC + 0.05f : hiC), 
+                    (cB == 3 || cB == 5 || cB == 6 ? lowC + 0.05f : hiC));
+        }
+    }
+
+    void resetDrop()
+    {
+        dropping = false;
+    }
+}
