65aeb1cc8722204167bef1c79e90f8bc2586725a
notsheepdog <93456568+notsheepdog@users.noreply.github.com>
2023-02-19 18:39:33 -0500

    Adding signature grading to signing minigame and also imported TMPro

---
+++ Friendly Fest/Assets/Scripts/Signing Minigame/Draw.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Draw : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject pen;
+
+    LineRenderer currentLineRenderer;
+
+    Vector2 lastPos;
+
+    private void Update()
+    {
+
+    }
+
+}
