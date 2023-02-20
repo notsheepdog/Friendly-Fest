ca6f4e47794b15849d81744604bc644b65a5cc46
Lucy! <89477983+ConsistentlyNervous@users.noreply.github.com>
2023-02-20 12:27:30 -0500

    Level Manager and Player Prefab made

---
+++ Friendly Fest/Assets/Scripts/Scriptable Objects/Level1.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Level1SO", menuName = "ScriptableObjects/Level1SO")]
+public class Level1 : ScriptableObject
+{
+    public bool paperSigned;
+}
