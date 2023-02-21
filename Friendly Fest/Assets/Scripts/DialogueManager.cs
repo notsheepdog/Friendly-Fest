ee962a7c9ccdc3ec602da6259bc5a6d3f0d21398
Dylan Dinio <DDinio2101@gmail.com>
2023-02-20 23:36:52 -0500

    Finalized movement and added UI canvas for dialogue and interactions

---
+++ Friendly Fest/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+    private string _name;
+    public Text _dialogue_text;
+    public Text _name_text;
+    public GameObject _textBox;
+    public KeyCode interact;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        this.sentences = new Queue<string>();
+        this._name = "";
+        this.gameObject.SetActive(false);
+
+    }
+
+
+    public void StartDialogue(DialogueSO dialogue)
+    {
+
+        this.sentences.Clear();
+        this._textBox.SetActive(true);
+        foreach (var sentence in dialogue.Sentences)
+        {
+            Debug.Log(sentence);
+            this.sentences.Enqueue(sentence);
+        }
+
+        this._name = dialogue.Name;
+        DisplayNextSentence();
+
+    }
+
+    public void DisplayNextSentence()
+    {
+        Debug.Log("next sentence");
+        if (sentences.Count == 0)
+        {
+            Debug.Log("ending the dialogue");
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        this._dialogue_text.text = sentence;
+        this._name_text.text = this._name;
+    }
+
+    public void EndDialogue()
+    {
+        this._textBox.SetActive(false);
+    }
+}
