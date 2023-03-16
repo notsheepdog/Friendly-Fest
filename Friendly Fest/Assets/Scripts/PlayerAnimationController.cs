2396167d2407907414dabdc3417fab5c2b0f5233
notsheepdog <93456568+notsheepdog@users.noreply.github.com>
2023-03-15 23:32:02 -0400

    Min Animation Finished

--- Friendly Fest/Assets/Scripts/PlayerAnimationController.cs
+++ Friendly Fest/Assets/Scripts/PlayerAnimationController.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class PlayerAnimationController : MonoBehaviour
 {
     // This script controls the animation states of Min
+    Animator animator;
 
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        
+        if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+        {
+            animator.SetInteger("State", 0);
+        }
+        else
+        {
+            animator.SetInteger("State", 1);
+        }
     }
 }
