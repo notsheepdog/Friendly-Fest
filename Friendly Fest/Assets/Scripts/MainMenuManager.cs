7fd5987562e6a451dd8e7fe2a7932d5c0cb11139
notsheepdog <93456568+notsheepdog@users.noreply.github.com>
2023-04-15 15:00:20 -0400

    Implemented persistent save data

--- Friendly Fest/Assets/Scripts/MainMenuManager.cs
+++ Friendly Fest/Assets/Scripts/MainMenuManager.cs
@@ -2,12 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenuManager : MonoBehaviour
 {
-    public void startNewGame()
+    public TextMeshProUGUI tasksCompleted;
+
+    private void Start()
+    {
+        Invoke("SetTasksCompleted", 0);
+    }
+
+    private void SetTasksCompleted()
+    {
+        tasksCompleted.text = "Tasks Completed: " + PersistentSaveData.TasksCompleted()
+            + " / " + PersistentSaveData.staticTasks.Length;
+    }
+
+    public void StartNewGame()
     {
         SceneManager.LoadScene(0);
+        PersistentSaveData.NewGame();
+    }
+
+    public void ContinueGame()
+    {
+        PersistentSaveData.ContinueGame();
+        SceneManager.LoadScene(PlayerPrefs.GetInt("sceneIndex"));
     }
 
 
